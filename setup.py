dc512150fa662659f62d01565c887aabe47a99a0
Riccardo Metere <rick@metere.it>
2016-04-10 15:01:20 +0200

    some fixes and preparing publication

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4',
     ],
 
     keywords='dicom dcm preprocessing',
